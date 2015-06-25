4b6f10a5eb0a6f06d6536778cfe65689a005d3b5
Jackson Willis <jackson@Jacksons-Air.pubhack.local>
2015-06-25 11:41:06 +0100

    adding new redirect url

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -14,7 +14,7 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '03ffe0cac0a0401aa6673c3cf6d02ced'; // Your client id
 var client_secret = 'a57c43efb9644574a96d6623fb8bfbc2'; // Your client secret
-var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
+var redirect_uri = 'http://jacksonwillis.com:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
